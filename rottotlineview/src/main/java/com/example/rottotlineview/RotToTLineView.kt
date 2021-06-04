ec278b2f80a5313e6ae10b517dbadbc131daa427
Anwesh43 <anweshthecool0@gmail.com>
2021-06-05 05:26:38 +0530

    adding RotToTLineView class

--- rottotlineview/src/main/java/com/example/rottotlineview/RotToTLineView.kt
+++ rottotlineview/src/main/java/com/example/rottotlineview/RotToTLineView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawRTTLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRotToTLine(scale, w, h, paint)
 }
+
+class RotToTLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
