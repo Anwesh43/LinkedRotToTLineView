6b308fbec37f2733251f9917ed08d8112a44292e
Anwesh43 <anweshthecool0@gmail.com>
2021-06-05 05:13:23 +0530

    adding constants and libraries

--- rottotlineview/src/main/java/com/example/rottotlineview/RotToTLineView.kt
+++ rottotlineview/src/main/java/com/example/rottotlineview/RotToTLineView.kt
@@ -1,2 +1,27 @@
 package com.example.rottotlineview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#673AB7",
+    "#00C853",
+    "#304FFE",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val tFactor : Float = 11.9f
+val lineFactor : Float = 3.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 45f
