ba31cece685a8bce2061d7811410fcd7f7b452c2
Anwesh43 <anweshthecool0@gmail.com>
2021-06-05 05:20:06 +0530

    adding Canvas extension methods

--- rottotlineview/src/main/java/com/example/rottotlineview/RotToTLineView.kt
+++ rottotlineview/src/main/java/com/example/rottotlineview/RotToTLineView.kt
@@ -30,3 +30,36 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawRotToTLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val tSize : Float = Math.min(w, h) / tFactor
+    val lSize : Float = Math.min(w, h) / lineFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        rotate(deg * (sf2 + sf4))
+        drawLine(0f, 0f, 0f, tSize * sf1, paint)
+        restore()
+    }
+    save()
+    translate(0f, h / 2 * (1 - sf3))
+    drawLine(0f, 0f, 0f, lSize, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawRTTLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRotToTLine(scale, w, h, paint)
+}
