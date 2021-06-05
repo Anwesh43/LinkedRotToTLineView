d9e84216272f5d553bd6ddca7dd04eb4b922ad78
Anwesh43 <anweshthecool0@gmail.com>
2021-06-05 05:40:52 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedrottotlineview/MainActivity.kt
+++ app/src/main/java/com/example/linkedrottotlineview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedrottotlineview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.rottotlineview.RotToTLineView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RotToTLineView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
